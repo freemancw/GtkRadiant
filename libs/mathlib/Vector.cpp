41755ea7239aa9139e3896d4253dbf1bcf9e03ae
freemancw <freeman@cs.unc.edu>
2012-12-26 22:40:04 -0500

    added constants

--- libs/mathlib/Vector.cpp
+++ libs/mathlib/Vector.cpp
@@ -9,11 +9,19 @@
 namespace RAD
 {
 
-    /*
+template<const size_t N>
+const Vector<N> Vector<N>::ZERO;
+
+const Vector2 Vector2::XAXIS(1.0f, 0.0f);
+const Vector2 Vector2::YAXIS(0.0f, 1.0f);
+
 const Vector3 Vector3::XAXIS(1.0f, 0.0f, 0.0f);
 const Vector3 Vector3::YAXIS(0.0f, 1.0f, 0.0f);
 const Vector3 Vector3::ZAXIS(0.0f, 0.0f, 1.0f);
-const Vector3 Vector3::ZERO(0.0f, 0.0f, 0.0f);
-*/
+
+const Vector4 Vector4::XAXIS(1.0f, 0.0f, 0.0f, 0.0f);
+const Vector4 Vector4::YAXIS(0.0f, 1.0f, 0.0f, 0.0f);
+const Vector4 Vector4::ZAXIS(0.0f, 0.0f, 1.0f, 0.0f);
+const Vector4 Vector4::WAXIS(0.0f, 0.0f, 0.0f, 1.0f);
 
 } // namespace RAD
